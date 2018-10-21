7dafa51b66281ba030ddfeec0921b7531373cfc7
tlburchh <37091749+tlburchh@users.noreply.github.com>
2018-10-21 17:15:15 -0400

    cleaned up folder structure and added material UI package

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -1,20 +1,20 @@
-import axios from "axios";
+// import axios from "axios";
 
-export default {
-  // Gets all books
-  getTrails: function() {
-    return axios.get("/api/trails");
-  },
-  // Gets the book with the given id
-  getTrail: function(id) {
-    return axios.get("/api/trails/" + id);
-  },
-  // Deletes the book with the given id
-  deleteTrail: function(id) {
-    return axios.delete("/api/trails/" + id);
-  },
-  // Saves a book to the database
-  saveTrail: function(trailData) {
-    return axios.post("/api/trails", trailData);
-  }
-};
+// export default {
+//   // Gets all books
+//   getTrails: function() {
+//     return axios.get("/api/trails");
+//   },
+//   // Gets the book with the given id
+//   getTrail: function(id) {
+//     return axios.get("/api/trails/" + id);
+//   },
+//   // Deletes the book with the given id
+//   deleteTrail: function(id) {
+//     return axios.delete("/api/trails/" + id);
+//   },
+//   // Saves a book to the database
+//   saveTrail: function(trailData) {
+//     return axios.post("/api/trails", trailData);
+//   }
+// };
