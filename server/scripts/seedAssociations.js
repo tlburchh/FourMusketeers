2ed4382579d51b01b639851cf56c8ba92b76a42d
Karsten @ Ubuntu on Dell Latitude e6430s <karstenrabe91@gmail.com>
2018-10-29 19:36:42 -0400

    Put seed DB associations script in package.json commands

--- server/scripts/seedAssociations.js
+++ server/scripts/seedAssociations.js
@@ -5,34 +5,31 @@ const db = require("../models");
 
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/wino", { useNewUrlParser: true });
 
-let userId;
+// let userId;
 let wineIds = [];
 let keywordIds = [];
-let ratingIds = [];
+// let ratingIds = [];
 
 
 // Save all the IDs in arrays
 db.User.find({}).then(users => {
     userId = users[0].id;
-    // console.log(userId);
 });
 
+// Get the wines, save em. Then Get the keywords and push a random wine into its array
+// Then push a keyword to each wine...
 db.Wines.find({}).then(wines => {
     wines.forEach(wine => {
         wineIds.push(wine.id);
     });
-});
-
-db.Keywords.find({}).then(keywords => {
-    keywords.forEach(kw => {
-        keywordIds.push(kw.id);
+    console.log(wineIds);
+    db.Keywords.find({}).then(keywords => {
+        keywords.forEach(keyword => {
+            keywordIds.push(keyword.id);
+        });
+        console.log(keywordIds);
         assKeys();
-    });
-});
-
-db.Rating.find({}).then(ratings => {
-    ratings.forEach(rating => {
-        ratingIds.push(rating.id);
+        addWineFlavors();
     });
 });
 
@@ -40,6 +37,11 @@ getRandomWine = () => {
     const len = wineIds.length;
     const rand = Math.floor(Math.random() * len);
     return wineIds[rand];
+}
+getRandomKw = () => {
+    const len = keywordIds.length;
+    const rand = Math.floor(Math.random() * len);
+    return keywordIds[rand];
 }
 
 assKeys = () => {
@@ -51,7 +53,19 @@ assKeys = () => {
                 }
             }
         ).then(result => {
-            console.log(result);
+        }).catch((err) => { console.log(err); })
+    });
+}
+// Put keywords in each wine's 'keywords' array
+addWineFlavors = () => {
+    wineIds.forEach(wine => {
+        db.Wines.findByIdAndUpdate(wine,
+            {
+                $push: {
+                    keywords: getRandomKw()
+                }
+            }
+        ).then(result => {
             process.exit(0);
         }).catch((err) => { console.log(err); })
     });
