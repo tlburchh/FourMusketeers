6e42db7ec083b0d75e44f2cfadb1d84dc649511a
tlburchh <37091749+tlburchh@users.noreply.github.com>
2018-10-30 21:17:07 -0400

    initial structure

---
+++ client/src/components/AdminWineEdit/AdminWineEdit.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import { withStyles } from '@material-ui/core/styles';
+import Grid from '@material-ui/core/Grid';
+import Paper from '@material-ui/core/Paper';
+import Typography from '@material-ui/core/Typography';
+
+class AdminWineEdit extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state={
+
+    };
+  }
+
+  render(){
+
+
+    return(
+      
+    )
+  }
+}
