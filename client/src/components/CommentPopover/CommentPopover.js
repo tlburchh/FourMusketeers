38b2ba7ae2ef12d16110a385c2e68b8ae88e391f
Karsten @ Ubuntu on Dell Latitude e6430s <karstenrabe91@gmail.com>
2018-11-03 11:40:13 -0400

    Add wine keywords to popover. Migrate deprecated typography to V2.

--- client/src/components/CommentPopover/CommentPopover.js
+++ client/src/components/CommentPopover/CommentPopover.js
@@ -1,66 +1,86 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { withStyles } from '@material-ui/core/styles';
+import { withStyles, createMuiTheme } from '@material-ui/core/styles';
 import Typography from '@material-ui/core/Typography';
 import Button from '@material-ui/core/Button';
 import Popover from '@material-ui/core/Popover';
-import toRenderProps from 'recompose/toRenderProps';
-import withState from 'recompose/withState';
 
-const WithState = toRenderProps(withState('anchorEl', 'updateAnchorEl', null));
-
-const styles = theme => ({
+window.__MUI_USE_NEXT_TYPOGRAPHY_VARIANTS__ = true;
+const theme = createMuiTheme ({
   typography: {
-    margin: theme.spacing.unit * 2,
-  },
+    margin: '2rem'
+  }
 });
 
-function CommentPopover(props) {
-  const { classes } = props;
+class CommentPopover extends React.Component {
 
-  return (
-    <WithState>
-      {({ anchorEl, updateAnchorEl }) => {
-        const open = Boolean(anchorEl);
-        return (
-          <React.Fragment>
-            <Button
-              aria-owns={open ? 'render-props-popover' : undefined}
-              aria-haspopup="true"
-              variant="contained"
-              onClick={event => {
-                updateAnchorEl(event.currentTarget);
-              }}
-            >
-              Comments
-            </Button>
-            <Popover
-              id="render-props-popover"
-              open={open}
-              anchorEl={anchorEl}
-              onClose={() => {
-                updateAnchorEl(null);
-              }}
-              anchorOrigin={{
-                vertical: 'center',
-                horizontal: 'center',
-              }}
-              transformOrigin={{
-                vertical: 'bottom',
-                horizontal: 'right',
-              }}
-            >
-              <Typography className={classes.typography}>The content of the Popover.</Typography>
-            </Popover>
-          </React.Fragment>
-        );
-      }}
-    </WithState>
-  );
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      anchorEl: null,
+    };
+  }
+
+  handleClick = event => {
+    this.setState({
+      anchorEl: event.currentTarget,
+    });
+  };
+
+  handleClose = () => {
+    this.setState({
+      anchorEl: null,
+    });
+  };
+
+  render() {
+    const { classes } = this.props;
+    const { anchorEl } = this.state;
+    const open = Boolean(anchorEl);
+    const keywds = this.props.keys;
+
+    return (
+      <React.Fragment>
+        <Button
+          aria-owns={open ? 'comment-popper' : undefined}
+          aria-haspopup="true"
+          variant="contained"
+          onClick={this.handleClick}
+        >
+          How was it?
+      </Button>
+        <Popover
+          id="comment-popper"
+          marginThreshold={20}
+          open={open}
+          onClose={this.handleClose}
+          anchorEl={anchorEl}
+          anchorOrigin={{
+            vertical: 'top',
+            horizontal: 'left',
+          }}
+          transformOrigin={{
+            vertical: 'top',
+            horizontal: 'center',
+          }}
+        >
+          <Typography className={classes.typography} >
+            {
+              keywds.map(k => {
+                return k.keyword
+              })
+            }
+          </Typography>
+        </Popover>
+      </React.Fragment>
+    );
+  }
 }
+
 
 CommentPopover.propTypes = {
   classes: PropTypes.object.isRequired,
 };
 
-export default withStyles(styles)(CommentPopover);
+export default withStyles(theme)(CommentPopover);
