1c366a7654902c5979eda67d4939362e64747a8b
Karsten @ Ubuntu on Dell Latitude e6430s <karstenrabe91@gmail.com>
2018-11-05 20:31:15 -0500

    Remove minor unused var errors from drag n drop bug

--- client/src/components/AdminWineEdit/AdminWineEdit.js
+++ client/src/components/AdminWineEdit/AdminWineEdit.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import ReactDOM from 'react-dom';
+// import ReactDOM from 'react-dom';
 import PropTypes from 'prop-types';
 import { withStyles } from '@material-ui/core/styles';
 import Grid from '@material-ui/core/Grid';
@@ -73,8 +73,7 @@ class AdminWineEdit extends Component {
 
     this.state = {
       data: [],
-      result: [],
-      data: [],
+      result: []
     };
     this.onDragEnd = this.onDragEnd.bind(this);
   }
