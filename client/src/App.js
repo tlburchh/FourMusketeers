bb5d228d29d68c2bb6a1cea5fb42dceb29aca0f9
Karsten @ Ubuntu on Dell Latitude e6430s <karstenrabe91@gmail.com>
2018-11-13 00:49:28 -0500

    Remove unused import

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Route, Switch, Redirect } from 'react-router-dom';
+import { Route, Switch } from 'react-router-dom';
 import LoginForm from './pages/Auth/LoginForm';
 import SignupForm from './pages/Auth/SignupForm';
 import Admin from './pages/Admin/Admin';
